015d7a4ea2a398a3197c211a16596b9f55f42891
Ludmil Paulo <53780546+ludmilpaulo@users.noreply.github.com>
2022-12-29 20:25:04 +0200

    front

---
+++ sd-web/src/App.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import  Login  from "./screens/Login";
+
+
+
+
+function App() {
+  return (
+  
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          
+        </Routes>
+      </Router>
+
+  );
+}
+
+export default App;
+
